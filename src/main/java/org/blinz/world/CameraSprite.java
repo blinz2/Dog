75502ebd1f2cb302f36a64d09773b4865b372a7c
Gary Talent <gtalent2@gmail.com>
2010-05-16 14:09:58 -0500

    Documented several methods in CameraSprite.

--- src/main/java/org/blinz/world/CameraSprite.java
+++ src/main/java/org/blinz/world/CameraSprite.java
@@ -32,31 +32,31 @@ class CameraSprite implements MouseListener {
     private boolean isSelected = false;
 
     CameraSprite(final BaseSprite sprite) {
-        this.sprite = sprite;
+	this.sprite = sprite;
     }
 
     @Override
     public void buttonClick(final int buttonNumber, final int numberOfClicks, final int cursorX, final int cursorY) {
-        if (sprite instanceof MouseListener) {
-            MouseListener listener = (MouseListener) sprite;
-            listener.buttonClick(buttonNumber, numberOfClicks, cursorX, cursorY);
-        }
+	if (sprite instanceof MouseListener) {
+	    MouseListener listener = (MouseListener) sprite;
+	    listener.buttonClick(buttonNumber, numberOfClicks, cursorX, cursorY);
+	}
     }
 
     @Override
     public void buttonPress(final int buttonNumber, final int cursorX, final int cursorY) {
-        if (sprite instanceof MouseListener) {
-            MouseListener listener = (MouseListener) sprite;
-            listener.buttonPress(buttonNumber, cursorX, cursorY);
-        }
+	if (sprite instanceof MouseListener) {
+	    MouseListener listener = (MouseListener) sprite;
+	    listener.buttonPress(buttonNumber, cursorX, cursorY);
+	}
     }
 
     @Override
     public void buttonRelease(final int buttonNumber, final int cursorX, final int cursorY) {
-        if (sprite instanceof MouseListener) {
-            MouseListener listener = (MouseListener) sprite;
-            listener.buttonRelease(buttonNumber, cursorX, cursorY);
-        }
+	if (sprite instanceof MouseListener) {
+	    MouseListener listener = (MouseListener) sprite;
+	    listener.buttonRelease(buttonNumber, cursorX, cursorY);
+	}
     }
 
     /**
@@ -64,7 +64,7 @@ class CameraSprite implements MouseListener {
      * @param sector the Sector that the sprite this CameraSprite represents belongs to
      */
     final void setSector(final Sector sector) {
-        this.sector = sector;
+	this.sector = sector;
     }
 
     /**
@@ -72,7 +72,7 @@ class CameraSprite implements MouseListener {
      * @return the Sector that the sprite this CameraSprite represents belongs to
      */
     final Sector getSector() {
-        return sector;
+	return sector;
     }
 
     /**
@@ -80,42 +80,66 @@ class CameraSprite implements MouseListener {
      * @return true if the contained sprite is selectable, false otherwise
      */
     final boolean isSelectable() {
-        return sprite instanceof SelectableSprite;
+	return sprite instanceof SelectableSprite;
     }
 
+    /**
+     * Gets the sprite this CameraSprite represents.
+     * @return the sprite this CameraSprite represents.
+     */
     final BaseSprite getSprite() {
-        return sprite;
+	return sprite;
     }
 
+    /**
+     * Gets the x coordinate of the sprite this represents
+     * @return the x coordinate of the sprite this represents
+     */
     final int getX() {
-        return sprite.getX();
+	return sprite.getX();
     }
 
+    /**
+     * Gets the y coordinate of the sprite this represents
+     * @return the y coordinate of the sprite this represents
+     */
     final int getY() {
-        return sprite.getY();
+	return sprite.getY();
     }
 
+    /**
+     * Gets the layer of the sprite this represents
+     * @return the layer of the sprite this represents
+     */
     final float getLayer() {
-        return sprite.getLayer();
+	return sprite.getLayer();
     }
 
+    /**
+     * Gets the width of the sprite this represents
+     * @return the width of the sprite this represents
+     */
     final int getWidth() {
-        return sprite.getWidth();
+	return sprite.getWidth();
     }
 
+    /**
+     * Gets the height of the sprite this represents
+     * @return the height of the sprite this represents
+     */
     final int getHeight() {
-        return sprite.getHeight();
+	return sprite.getHeight();
     }
 
     final void draw(final Graphics graphics, final Bounds bounds) {
-        sprite.draw(graphics, bounds);
-        if (sprite instanceof SelectableSprite && isSelected()) {
-            ((SelectableSprite) sprite).drawSelectionIndicator(graphics, bounds);
-        }
+	sprite.draw(graphics, bounds);
+	if (sprite instanceof SelectableSprite && isSelected()) {
+	    ((SelectableSprite) sprite).drawSelectionIndicator(graphics, bounds);
+	}
     }
 
     boolean isMouseListener() {
-        return sprite instanceof MouseListener;
+	return sprite instanceof MouseListener;
     }
 
     /**
@@ -123,10 +147,10 @@ class CameraSprite implements MouseListener {
      * @param user the User that selected this sprite
      */
     final void select(final User user) {
-        if (sprite instanceof SelectableSprite) {
-            ((SelectableSprite) sprite).select(user);
-            isSelected = true;
-        }
+	if (sprite instanceof SelectableSprite) {
+	    ((SelectableSprite) sprite).select(user);
+	    isSelected = true;
+	}
     }
 
     /**
@@ -134,13 +158,13 @@ class CameraSprite implements MouseListener {
      * @param user the User that deselected this sprite
      */
     final void deselect(final User user) {
-        if (sprite instanceof SelectableSprite) {
-            ((SelectableSprite) sprite).deselect(user);
-            isSelected = false;
-        }
+	if (sprite instanceof SelectableSprite) {
+	    ((SelectableSprite) sprite).deselect(user);
+	    isSelected = false;
+	}
     }
 
     boolean isSelected() {
-        return isSelected;
+	return isSelected;
     }
 }
