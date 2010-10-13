c8dc360eabe370b8d46cffe9043f7e09b3c30d9c
Gary Talent <gtalent2@gmail.com>
2010-10-13 09:32:08 -0500

    Renamed the project to Dog.

--- packageDist.sh
+++ packageDist.sh
@@ -1,6 +1,6 @@
 mvn clean package
 mkdir dist
-mv target/BlinzEngine*.jar dist/BlinzEngine.jar
+mv target/Dog*.jar dist/Dog.jar
 mvn javadoc:javadoc
 mv target/site/apidocs dist/javadoc
 cp -r src/main/java dist/src
