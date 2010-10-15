529a198c8060ab5880a06ecfaf420ca7275da541
Gary Talent <gtalent2@gmail.com>
2010-10-14 23:38:00 -0500

    Changed from GPL 3 to GPL 2.

--- src/main/java/net/blinz/dog/world/Sector.java
+++ src/main/java/net/blinz/dog/world/Sector.java
@@ -1,18 +1,18 @@
 /*
- *  BlinzEngine - A library for large 2D world simultions and games.
- *  Copyright (C) 2009 - 2010  Blinz <gtalent2@gmail.com>
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 3 as
- *  published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Dog - A project for making highly scalable non-clustered game and simulation environments.
+ * Copyright (C) 2009-2010 BlinzProject <gtalent2@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.blinz.dog.world;
 
